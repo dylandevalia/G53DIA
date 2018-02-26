82314c08260053d43cc1ca99b34abdc35d480722
Dylan Devalia <dylan@devalia.com>
2018-02-26 21:56:20 +0000

    Updated formatting

--- src/uk/ac/nott/cs/g53dia/library/TankerViewerIconFactory.java
+++ src/uk/ac/nott/cs/g53dia/library/TankerViewerIconFactory.java
@@ -1,5 +1,7 @@
 package uk.ac.nott.cs.g53dia.library;
+
 import javax.swing.Icon;
+
 /**
  * Interface allowing customisation of the {@link TankerViewer} icons used to depict objects in the environment
  * different types of Tankers and cells.
@@ -9,26 +11,26 @@ import javax.swing.Icon;
 /*
  * Copyright (c) 2003 Stuart Reeves
  * Copyright (c) 2003-2005 Neil Madden (nem@cs.nott.ac.uk).
- * 
+ *
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  */
 public interface TankerViewerIconFactory {
 
-    /**
-     * Select a suitable icon for the given cell.
-     *
-     * @param cell The cell to select an icon for.
-     * @return An icon representing the cell.
-     */
-    public Icon getIconForCell(Cell cell);
+	/**
+	 * Select a suitable icon for the given cell.
+	 *
+	 * @param cell The cell to select an icon for.
+	 * @return An icon representing the cell.
+	 */
+	public Icon getIconForCell(Cell cell);
 
-    /**
-     * Select an icon for a tanker.
-     *
-     * @param tanker The tanker to select an icon for.
-     * @return An icon representing the tanker.
-     */
-    public Icon getIconForTanker(Tanker tanker);
+	/**
+	 * Select an icon for a tanker.
+	 *
+	 * @param tanker The tanker to select an icon for.
+	 * @return An icon representing the tanker.
+	 */
+	public Icon getIconForTanker(Tanker tanker);
 
 }
